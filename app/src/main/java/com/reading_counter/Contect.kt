327ea953b180c66d33fea0f6bfd219d0fd4dbb03
AndreyPeretyatko <118631517+AndreyPeretyatko@users.noreply.github.com>
2022-11-27 21:39:25 +0500

    Initial commit

---
+++ app/src/main/java/com/reading_counter/Contect.kt
@@ -0,0 +1,9 @@
+package com.reading_counter
+
+import com.reading_counter.Frgments.AddNewFrag
+
+
+lateinit var MAIN: MainActivity
+lateinit var ADDNEWFRAG: AddNewFrag
+
+val List_Size: String = "List_Size"
